d992346ec0e7c193ae75134090edadc872244f0d
elfotito <49953276+elfotito@users.noreply.github.com>
2026-08-04 14:43:36 -0400

    Actualizar descuentos.routes.js

--- src/routes/descuentos.routes.js
+++ src/routes/descuentos.routes.js
@@ -6,15 +6,14 @@ import {
   editarDescuento,
   eliminarDescuento,
 } from '../controllers/descuentos.controller.js';
-import verifyJWT from '../middleware/verifyJWT.js'; // ajusta el nombre real si difiere
-import soloAdmin from '../middleware/soloAdmin.middleware.js';
+import { verifyJWT, verifyAdmin } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/', verifyJWT, soloAdmin, listarDescuentos);
-router.get('/producto/:id', verifyJWT, soloAdmin, historialPorProducto);
-router.post('/', verifyJWT, soloAdmin, crearDescuento);
-router.put('/:id', verifyJWT, soloAdmin, editarDescuento);
-router.delete('/:id', verifyJWT, soloAdmin, eliminarDescuento);
+router.get('/', verifyJWT, verifyAdmin, listarDescuentos);
+router.get('/producto/:id', verifyJWT, verifyAdmin, historialPorProducto);
+router.post('/', verifyJWT, verifyAdmin, crearDescuento);
+router.put('/:id', verifyJWT, verifyAdmin, editarDescuento);
+router.delete('/:id', verifyJWT, verifyAdmin, eliminarDescuento);
 
 export default router;
