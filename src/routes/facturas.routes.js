05aefc89c2499375ba90060073d33f365e7f9619
elfotito <49953276+elfotito@users.noreply.github.com>
2026-08-15 11:54:54 -0400

    Actualizar facturas.routes.js

--- src/routes/facturas.routes.js
+++ src/routes/facturas.routes.js
@@ -3,6 +3,7 @@ import {
   getFacturas,
   createFactura,
   updateFactura,
+  deleteFactura,
   getOrdenesSinFacturar
 } from '../controllers/facturas.controller.js';
 import { verifyJWT, verifyAdmin } from '../middleware/auth.js';
@@ -14,6 +15,7 @@ router.use(verifyJWT, verifyAdmin);
 router.get('/', getFacturas);
 router.post('/', createFactura);
 router.patch('/:id', updateFactura);
+router.delete('/:id', deleteFactura);
 router.get('/sin-facturar/:usuario_id', getOrdenesSinFacturar);
 
 export default router;
