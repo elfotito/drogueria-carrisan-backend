6d76dbea0a6039864825ec8f7fe3c6c6ba5d5632
elfotito <49953276+elfotito@users.noreply.github.com>
2026-08-24 21:39:00 -0400

    Actualizar analytics.routes.js

--- src/routes/analytics.routes.js
+++ src/routes/analytics.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { getVentasPorPeriodo } from '../controllers/analytics.controller.js';
 import { verifyJWT, verifyAdmin } from '../middleware/auth.js';
+import { getVentasPorPeriodo, getEstadoCuentaClientes } from '../controllers/analytics.controller.js';
 
 const router = Router();
 
 router.get('/ventas', verifyJWT, verifyAdmin, getVentasPorPeriodo);
+router.get('/clientes', verifyJWT, verifyAdmin, getEstadoCuentaClientes);
 
 export default router;
