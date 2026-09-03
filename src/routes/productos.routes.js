2af37565d689beb98d671b9810060d32932d8d33
elfotito <49953276+elfotito@users.noreply.github.com>
2026-09-03 08:24:35 -0400

    Actualizar productos.routes.js

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -7,13 +7,6 @@ import {
   updateProducto
 } from '../controllers/productos.controller.js';
 import { verifyJWT, verifyAdmin } from '../middleware/auth.js';
-import {
-  getEstadoAlerta,
-  suscribirseAlerta,
-  cancelarAlerta,
-} from '../controllers/alertasDisponibilidad.controller.js';
-
-
 
 const router = Router();
 
@@ -22,8 +15,5 @@ router.get('/metadata', getProductosMetadata);
 router.get('/:id', getProductoById);
 router.post('/', verifyJWT, verifyAdmin, createProducto);
 router.patch('/:id', verifyJWT, verifyAdmin, updateProducto);
-router.get('/:id/avisame', verifyJWT, getEstadoAlerta);
-router.post('/:id/avisame', verifyJWT, suscribirseAlerta);
-router.delete('/:id/avisame', verifyJWT, cancelarAlerta);
 
 export default router;
